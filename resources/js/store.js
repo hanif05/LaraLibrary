7e659d8097d1b4dc19e04d54fd8da458a1be6acb
hanif05 <muamarhanif05@gmail.com>
2019-08-28 13:00:18 +0700

    Templating & make Authentification

--- resources/js/store.js
+++ resources/js/store.js
@@ -6,28 +6,26 @@ import auth from './stores/auth.js'
 
 Vue.use(Vuex)
 
-//DEFINE ROOT STORE VUEX
+//define root store vuex
 const store = new Vuex.Store({
-    //SEMUA MODULE YANG DIBUAT AKAN DITEPATKAN DIDALAM BAGIAN INI DAN DIPISAHKAN DENGAN KOMA UNTUK SETIAP MODULE-NYA
+
     modules: {
         auth
     },
-  	//STATE HAMPIR SERUPA DENGAN PROPERTY DATA DARI COMPONENT HANYA SAJA DAPAT DIGUNAKAN SECARA GLOBAL
+  	
     state: {
-        //VARIABLE TOKEN MENGAMBIL VALUE DARI LOCAL STORAGE token
+        
         token: localStorage.getItem('token'),
         errors: []
     },
     getters: {
-        //KITA MEMBUAT SEBUAH GETTERS DENGAN NAMA isAuth
-        //DIMANA GETTERS INI AKAN BERNILAI TRUE/FALSE DENGAN KONDISI BERDASARKAN
-        //STATE token.
+      
         isAuth: state => {
             return state.token != "null" && state.token != null
         }
     },
     mutations: {
-        //SEBUAH MUTATIONS YANG BERFUNGSI UNTUK MEMANIPULASI VALUE DARI STATE token
+       
         SET_TOKEN(state, payload) {
             state.token = payload
         },
--- resources/js/stores/auth.js
+++ resources/js/stores/auth.js
@@ -10,27 +10,18 @@ const mutations = {
 
 const actions = {
     submit({ commit }, payload) {
-        localStorage.setItem('token', null) //RESET LOCAL STORAGE MENJADI NULL
-        commit('SET_TOKEN', null, { root: true }) //RESET STATE TOKEN MENJADI NULL
-        //KARENA MUTATIONS SET_TOKEN BERADA PADA ROOT STORES, MAKA DITAMBAHKAN PARAMETER
-        //{ root: true }
+        localStorage.setItem('token', null) 
+        commit('SET_TOKEN', null, { root: true }) 
       
-        //KITA MENGGUNAKAN PROMISE AGAR FUNGSI SELANJUTNYA BERJALAN KETIKA FUNGSI INI SELESAI
         return new Promise((resolve, reject) => {
-            //MENGIRIM REQUEST KE SERVER DENGAN URI /login 
-            //DAN PAYLOAD ADALAH DATA YANG DIKIRIMKAN DARI COMPONENT LOGIN.VUE
             $axios.post('/login', payload)
             .then((response) => {
-                //KEMUDIAN JIKA RESPONNYA SUKSES
                 if (response.data.status == 'success') {
-                    //MAKA LOCAL STORAGE DAN STATE TOKEN AKAN DISET MENGGUNAKAN
-                    //API DARI SERVER RESPONSE
                     localStorage.setItem('token', response.data.data)
                     commit('SET_TOKEN', response.data.data, { root: true })
                 } else {
                     commit('SET_ERRORS', { invalid: 'Email/Password Salah' }, { root: true })
                 }
-                //JANGAN LUPA UNTUK MELAKUKAN RESOLVE AGAR DIANGGAP SELESAI
                 resolve(response.data)
             })
             .catch((error) => {
